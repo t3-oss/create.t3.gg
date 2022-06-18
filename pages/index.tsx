3d858364d240e033f3b3c82332a7e4ff7b3fdaf8
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-18 15:47:11 +0530

    increase padding again

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
         <div className="pt-12 md:pt-6" />
         <code
           id="command"
-          className="px-2 py-1 text-base text-blue-300 rounded-md cursor-pointer bg-neutral-800"
+          className="px-3 py-1.5 text-base text-blue-300 rounded-md cursor-pointer bg-neutral-800"
           onClick={() => {
             navigator.clipboard.writeText("npx create-t3-app@latest");
 
