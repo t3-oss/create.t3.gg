d35738b293ed604b9cd58f5ff333f43ee84c3b48
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-27 22:59:23 +0530

    add anim

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,14 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-[#E8DCFF] text-5xl font-bold text-center">
+        <h1 className="text-[#E8DCFF] text-5xl font-bold text-center anim-1">
           Create t3 App
         </h1>
 
         <div className="pt-12 md:pt-6" />
         <code
           id="command"
-          className="px-3 py-1.5 text-base text-blue-300 rounded-md cursor-pointer bg-neutral-800 hover:bg-neutral-700 transition-all duration-300"
+          className="px-3 py-1.5 text-base text-blue-300 rounded-md cursor-pointer bg-neutral-800 hover:bg-neutral-700 transition-all duration-300 anim-2"
           onClick={() => {
             navigator.clipboard.writeText("npx create-t3-app@latest");
 
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
         </code>
 
         <div className="mt-4" />
-        <div className="flex gap-4 font-medium text-neutral-300">
+        <div className="flex gap-4 font-medium text-neutral-300 anim-2">
           <a
             href="https://github.com/nexxeln/create-t3-app"
             rel="noreferrer"
