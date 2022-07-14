0fb282d8e76d52a0c5d27b234cfa36dc5ecf83f1
nuro <4991309+NuroDev@users.noreply.github.com>
2022-07-14 10:42:03 +0000

    Added new project base using Astro

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
