487f9fc4c233aa7c80afdce324372b37ec6c9d41
Guillermo Cava <gcavanunez@gmail.com>
2022-07-27 12:21:50 -0500

    chore: only highlight changed parts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import toast, { Toaster } from 'react-hot-toast';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import toast, { Toaster } from "react-hot-toast";
 
 const Home: NextPage = () => {
   return (
